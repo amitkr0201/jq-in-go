ee5269f1aff0818f17d0368799974218e7ee58b8
Amit Kumar <akumar105@sapient.com>
2018-04-26 17:12:22 +0100

    Add license and use Cobra framework

---
+++ cmd/jqingo/jqingo_test.go
@@ -0,0 +1,137 @@
+package jqingo
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestProcessCommand(t *testing.T) {
+	type args struct {
+		compact bool
+		args    []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Passing non compact JSON",
+			args: args{
+				args:    []string{`{"Test1":true,"Test2":"Yes"}`},
+				compact: false,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Passing print compact JSON",
+			args: args{
+				args:    []string{`{"Test1": true,"Test2": "Yes"}`},
+				compact: true,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Failing JSON",
+			args: args{
+				args:    []string{`{"Test1": true,"Test2": "Yes",}`},
+				compact: false,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := ProcessCommand(tt.args.compact, tt.args.args); (err != nil) != tt.wantErr {
+				t.Errorf("ProcessCommand() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_formatOutput(t *testing.T) {
+	type args struct {
+		compact bool
+		input   string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		{
+			name: "Print non compact JSON",
+			args: args{
+				input:   `{"Test1":true,"Test2":"Yes"}`,
+				compact: false,
+			},
+			want: []byte(`{
+  "Test1": true,
+  "Test2": "Yes"
+}`),
+			wantErr: false,
+		},
+		{
+			name: "Print compact  JSON",
+			args: args{
+				input: `{
+  "Test1": true,
+  "Test2": "Yes"
+				  }`,
+				compact: true,
+			},
+			want:    []byte(`{"Test1":true,"Test2":"Yes"}`),
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := formatOutput(tt.args.compact, tt.args.input)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("formatOutput() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("formatOutput() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_printOutput(t *testing.T) {
+	type args struct {
+		input []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// Can't easily fail this
+		{
+			name:    "Test non compact JSON",
+			wantErr: false,
+			args: args{
+				input: []byte(`{
+  "Test1": true,
+  "Test2": "Yes"
+}`),
+			},
+		},
+		{
+			name:    "Test compact JSON",
+			wantErr: false,
+			args: args{
+				input: []byte(`{"Test1": true,"Test2": "Yes"}`),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := printOutput(tt.args.input); (err != nil) != tt.wantErr {
+				t.Errorf("printOutput() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
